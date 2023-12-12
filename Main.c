e33ce4d6d50376d9a7a53d76388171e67f9de66c
Bryan Hdz <bryanhzaz@gmail.com>
2023-12-12 11:35:14 -0600

    The main function to develop.

---
+++ Main.c
@@ -0,0 +1,11 @@
+/*Proyect: Inventory Management. Buil a basic inventory management system for a store.
+Allow users add products, update stock y generate inventory reports.
+Author: Bryan Hernandez A.
+Delivery date: 8 January 2024*/
+#include <stdio.h>
+
+//Main:
+int main()
+{
+
+}
